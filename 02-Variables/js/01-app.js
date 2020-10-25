02b08eb1e0c75eb11608f10f28cc5293028c268f
Angel Alegre <fisiangel.14@gmail.com>
2020-10-25 18:41:07 -0500

    Recomendaciones para JS

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,25 @@
+//Inicializar una veriable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+//Se pueden reasignar
+producto = "Monitor de 26 pulgadas";
+console.log(producto);
+//no es un leng. tipado
+producto = 20;
+console.log(producto);
+
+//Se pueden inicializar sin valor
+var disponible;
+
+disponible = true;
+disponible = false;
+
+//crar varias variables a la vez
+var categoria = 'Computadora',
+    marca = 'Marca Kingtong',
+    calificacion = 8;
+
+//para nombres variables de mas de 1 pabalabra y no deben empezar con numeros
+var nombreProducto;
+var nombre_producto_variable;
+
