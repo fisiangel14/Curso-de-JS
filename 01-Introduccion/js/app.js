9e998863347455194a022d0b8ee44b35fdc17a1e
Angel Alegre <fisiangel.14@gmail.com>
2020-10-25 13:07:01 -0500

    Recomendaciones para JS

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -5,7 +5,19 @@ document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo J
 
 //comentarios en una linea
 //const producto = 'Monitor de 24 pulgadas';
-
+/*
 const nombre = prompt('Cual es tu nombre?');
 //muestra el nombre ingresado
 document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo JavaScript Moderno`;
+*/
+
+//REcomendado ; al final y 1 instruccion x linea
+console.log('Hola');
+console.log('Mundo');
+
+//funciones
+function hola(){
+    console.log("Esta es una funcion");
+}
+
+hola();
