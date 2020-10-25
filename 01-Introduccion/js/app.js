32a5a544e7601046a9e15181200108b4310f3607
Angel Alegre <fisiangel.14@gmail.com>
2020-10-25 12:44:03 -0500

    comentarios en js

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,2 +1,11 @@
+/*comentarios en varias lineas
 const nombre = prompt('Cual es tu nombre?');
+document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo JavaScript Moderno`;
+*/
+
+//comentarios en una linea
+//const producto = 'Monitor de 24 pulgadas';
+
+const nombre = prompt('Cual es tu nombre?');
+//muestra el nombre ingresado
 document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo JavaScript Moderno`;
