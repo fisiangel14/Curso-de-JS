a20a2201acfd4fa5f6217a569bac98d98f9b2e6a
Angel Alegre <fisiangel.14@gmail.com>
2020-10-25 22:13:46 -0500

    String 01

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,10 @@
+//Declaracion de un String
+const producto = "Monitor de 20\"";
+//2da forma de declarar
+const producto2 = String('Monitor de 24"');
+//Forma casi rara de declarar
+const producto3 = new String('Monitor de 26 pulgadas');
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
