26a3b9c85174db703e28b1b31b62ebeb99aa7f29
Angel Alegre <fisiangel.14@gmail.com>
2020-10-25 19:03:17 -0500

    variables 02

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,15 @@
+let producto = 'tablet';
+
+producto = 'monitor';
+
+producto = 20;
+producto = true;
+producto = null;
+
+console.log(producto);
+
+let precio;
+
+precio = 20;
+
+console.log(precio);
