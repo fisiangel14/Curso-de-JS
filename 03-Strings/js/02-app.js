42d317f27220ef549fe91b689e13066403ed954d
Angel Alegre <fisiangel.14@gmail.com>
2020-10-26 09:34:24 -0500

    String 02

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,11 @@
+const producto = "Monitor de20 pulgadas de 4k";
+
+console.log(producto);
+
+//cobtener el tamaño del string
+console.log(producto.length);
+
+//para encontrar el elemento, devuelve la posicion donde lo encuentra
+console.log(producto.indexOf('de'));
+//para encontrar el elemento, devuelve true o false si lo encuentra o no
+console.log(producto.includes('Monitor'))
